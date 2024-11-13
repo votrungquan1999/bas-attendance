2b15dd034ea852b66c773497c94b9854a5aac5ab
Quan Vo <votrungquan99@gmail.com>
2024-11-13 22:11:09 +0700

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
-		<div>
-			<h1>BasAttendance</h1>
-		</div>
+		<main className="flex flex-col items-center justify-center flex-1">
+			<h1 className="text-2xl md:text-4xl font-bold mb-6 text-center px-4">
+				Welcome to Basketball Attendance
+			</h1>
+			<Link
+				href="/attendance"
+				className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+			>
+				Go to Attendance Page
+			</Link>
+		</main>
 	);
 }
