9ee3cce3636963d68d0a265cf38497547562872f
Quan Vo <votrungquan99@gmail.com>
2024-11-17 23:02:42 +0700

    feat: switch user button

--- src/app/attendance/activity/page.tsx
+++ src/app/attendance/activity/page.tsx
@@ -43,6 +43,14 @@ export default async function ActivityPage() {
 					<h2 className="text-xl text-gray-600 font-inter">
 						What have you trained today?
 					</h2>
+					<form action={action_resetTaker} className="mt-4">
+						<button
+							type="submit"
+							className="text-sm text-gray-500 hover:text-gray-700 underline"
+						>
+							Not {athlete.value}? Click here to switch user
+						</button>
+					</form>
 				</div>
 			</header>
 
