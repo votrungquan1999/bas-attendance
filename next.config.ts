37de9c8d5a9ebb5cb6ca491cdd455a8ddec15d63
Quan Vo <votrungquan99@gmail.com>
2024-12-06 23:44:35 +0700

    fix: mokey fix netlify issue

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
 	/* config options here */
 	experimental: {
 		dynamicIO: true,
+		serverActions: {
+			allowedOrigins: [
+				"basketball-attendance.netlify.app",
+				"proxy.proxy-production.svc.cluster.local",
+			],
+		},
 	},
 };
 
