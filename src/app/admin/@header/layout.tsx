b64de203a9b11d61b629148e6ee7c279f6f898c6
Quan Vo <votrungquan99@gmail.com>
2024-12-12 23:40:54 +0700

    feat: better header

---
+++ src/app/admin/@header/layout.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import UserPopover from "src/components/next_auth/UserPopover";
+import UserPopoverSkeleton from "src/components/next_auth/UserPopoverSkeleton";
+
+export default function AdminHeader({
+	children,
+}: { children: React.ReactNode }) {
+	return (
+		<header className="flex h-[72px] w-full items-center border-b border-slate-200 bg-white shadow">
+			{children}
+			<div className="flex-1" />
+			<div className="px-6">
+				<Suspense fallback={<UserPopoverSkeleton />}>
+					<UserPopover />
+				</Suspense>
+			</div>
+		</header>
+	);
+}
