5eada20dbf8249255aef52cfcb8c071d29e3c374
Quan Vo <votrungquan99@gmail.com>
2024-12-22 00:30:28 +0700

    test: clean up fns for test

--- src/helpers/withTestContext.ts
+++ src/helpers/withTestContext.ts
@@ -1,5 +1,6 @@
 import { mock } from "bun:test";
 import withTestMongoDB from "./withTestMongoDB";
+import { cleanUp as cleanUpMongoDB } from "./withTestMongoDB";
 
 export default function withTestContext(fn: () => void): () => void {
 	// Mock Next.js cache functions
@@ -8,18 +9,31 @@ export default function withTestContext(fn: () => void): () => void {
 		unstable_cacheTag: mock(() => {}),
 	}));
 
-	// Mock query_getEligibleAthletes
+	return withTestMongoDB(fn);
+}
+
+export async function cleanUp() {
+	await cleanUpMongoDB();
+}
+
+export async function mockGetEligibleAthletes() {
 	mock.module(
 		"src/app/admin/attendance/by-week/query_getEligibleAthletes",
 		() => ({
 			default: async () => [
 				{
 					id: "athlete1",
-					value: "Test Athlete",
+					value: "Test Athlete 1",
+				},
+				{
+					id: "athlete2",
+					value: "Test Athlete 2",
+				},
+				{
+					id: "athlete3",
+					value: "Test Athlete 3",
 				},
 			],
 		}),
 	);
-
-	return withTestMongoDB(fn);
 }
