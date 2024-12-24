1419317ff2dcd232896ce0b69228ff64e88059da
Quan Vo <votrungquan99@gmail.com>
2024-12-24 15:07:25 +0700

    chore: add achievement collection

--- src/server/collections.ts
+++ src/server/collections.ts
@@ -1,11 +1,14 @@
 import type { CompletedActivity, WeeklyGoals } from "src/server/types";
 import type { Brand } from "utility-types";
+import type { AchievementState } from "./queries/achievementReducer";
 
 export type ActivitiesCollectionDocument = CompletedActivity;
 
 export const ActivitiesCollectionName = "activities";
 
 export const WeeklyGoalsCollectionName = "weekly_goals";
+
+export const AchievementsCollectionName = "achievements";
 
 export type WeeklyGoalsCollectionDocument = {
 	id: string;
@@ -17,6 +20,14 @@ export type WeeklyGoalsCollectionDocument = {
 
 	updatedAt: number;
 	updatedBy: string;
+};
+
+export type AchievementCollectionDocument = {
+	id: string;
+	athleteId: string;
+	state: AchievementState;
+	lastActivityId: string | null;
+	updatedAt: number;
 };
 
 export type WeekId = Brand<string, "WeekId">;
